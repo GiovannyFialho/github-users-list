0df6210af45dc00b35ec3c6365158340a7afd3d0
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-10-10 19:30:34 -0300

    ajustando carregamento

--- app/providers/index.tsx
+++ app/providers/index.tsx
@@ -1,10 +1,20 @@
 "use client";
 
-import { type ReactNode } from "react";
-import { I18nextProvider } from "react-i18next";
+import { useEffect, useState, type ReactNode } from "react";
+import { I18nextProvider, useTranslation } from "react-i18next";
 
 import i18next from "@/app/i18n";
 
 export function Providers({ children }: { children: ReactNode }) {
+  const { i18n } = useTranslation();
+
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setIsReady(i18n.isInitialized);
+  }, [i18n]);
+
+  if (!isReady) return null;
+
   return <I18nextProvider i18n={i18next}>{children}</I18nextProvider>;
 }
