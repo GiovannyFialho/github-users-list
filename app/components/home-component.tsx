e2dcd2c8f7279d9eb37f5562ab0714aeddfac20f
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-10-14 19:06:06 -0300

    ajustes mobile

--- app/components/home-component.tsx
+++ app/components/home-component.tsx
@@ -17,7 +17,7 @@ export default function HomeComponent() {
   }, [searchUser]);
 
   return (
-    <div className="w-full min-h-[calc(100vh-64px)] flex items-center justify-center">
+    <div className="w-full min-h-[calc(100vh-64px)] flex md:items-center justify-center mt-10 md:mt-0">
       <div className="max-w-5xl w-full flex flex-col gap-3 px-5">
         <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold text-center text-primary mb-5">
           {t("Home.title")}
