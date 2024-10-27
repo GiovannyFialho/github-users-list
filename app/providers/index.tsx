d1b805be1f7a72700139be3461ca0995e626bb94
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-10-27 16:09:38 -0300

    ajustando carregamento dos providers

--- app/providers/index.tsx
+++ app/providers/index.tsx
@@ -3,7 +3,7 @@
 import i18next from "@/app/i18n";
 import { ApolloProvider } from "@apollo/client";
 import { SessionProvider } from "next-auth/react";
-import { useEffect, useState, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import { I18nextProvider, useTranslation } from "react-i18next";
 import { RotatingLines } from "react-loader-spinner";
 
@@ -11,6 +11,7 @@ import { ThemeProvider } from "@/app/context/ThemeContext";
 import { UserProfileProvider } from "@/app/context/UserProfileContext";
 
 import { client } from "@/app/lib/client";
+
 import tailwindConfig from "@/tailwind.config";
 
 type ProvidersProps = {
@@ -19,18 +20,9 @@ type ProvidersProps = {
 };
 
 export function Providers({ children, initialTheme }: ProvidersProps) {
-  const { i18n } = useTranslation();
-  const [isReady, setIsReady] = useState(false);
+  const { ready } = useTranslation();
 
-  useEffect(() => {
-    const initializeI18n = async () => {
-      await i18n.init();
-      setIsReady(true);
-    };
-    initializeI18n();
-  }, [i18n]);
-
-  if (!isReady) {
+  if (!ready) {
     return (
       <div className="w-screen h-screen flex items-center justify-center">
         <RotatingLines
